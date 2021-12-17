e44b0947aa97af04643677dae059f250f1ffec07
Andrei <andreim.graur@gmail.com>
2021-12-17 12:20:15 +0200

    Reworked product controller

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -1,15 +1,16 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.ShoppingCartDao;
 import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.dao.implementation.ShoppingCartDaoMem;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.service.ProductService;
-import com.codecool.shop.config.TemplateEngineUtil;
-import com.codecool.shop.model.Product;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -19,66 +20,47 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @WebServlet(urlPatterns = {"/"})
 public class ProductController extends HttpServlet {
 
-
-
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        String page = req.getParameter("category");
+        String supplier = req.getParameter("supplier");
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        SupplierDao supplierDaoDataStore = SupplierDaoMem.getInstance();
-        ProductService productService = new ProductService(productDataStore,productCategoryDataStore, supplierDaoDataStore);
-        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        SupplierDao supplierDao = SupplierDaoMem.getInstance();
+        ShoppingCartDao shoppingCartDao = ShoppingCartDaoMem.getInstance();
+        ProductService productService = new ProductService(productDataStore,productCategoryDataStore, shoppingCartDao);
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        context.setVariable("category", productCategoryDataStore.getAll());
-        context.setVariable("products", productDataStore.getAll());
-        context.setVariable("supplier", supplierDataStore.getAll());
 
-        // // Alternative setting of the template context
-        // Map<String, Object> params = new HashMap<>();
-        // params.put("category", productCategoryDataStore.find(1));
-        // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-        // context.setVariables(params);
+        if (page != null) {
+            for (ProductCategory pd : productCategoryDataStore.getAll()) {
+                if (page.equals(pd.getName().toLowerCase())) {
+                    System.out.println(pd.toString());
+                    context.setVariable("category", productService.getProductCategory(pd.getId()));
+                    context.setVariable("products", productService.getProductsForCategory(pd.getId()));
+                } else {
+                    context.setVariable("category", productService.getProductCategory(1));
+                    context.setVariable("products", productService.getProductsForCategory(1));
+                }
+            }
+        } else if (supplier != null){
+            context.setVariable("products", productService.getSupplierDao(supplier));
+            context.setVariable("currentSupplier", supplier);
+        } else {
+            context.setVariable("category", productService.getProductCategory(1));
+            context.setVariable("products", productService.getProductsForCategory(1));
+        }
+        context.setVariable("supplierList", supplierDao.getAll());
+
+        context.setVariable("categories", productCategoryDataStore.getAll());
         engine.process("product/index.html", context, resp.getWriter());
-    }
-
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-        String category = request.getParameter("category");
-        String supplier = request.getParameter("supplier");
-
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        SupplierDao supplierDaoDataStore = SupplierDaoMem.getInstance();
-        ProductService productService = new ProductService(productDataStore,productCategoryDataStore, supplierDaoDataStore);
-        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
-
-        if (category != null) {
-            TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
-            WebContext context = new WebContext(request, response, request.getServletContext());
-            context.setVariable("category", productCategoryDataStore.getAll());
-            context.setVariable("products", productService.getProductsForCategory(Integer.parseInt(category)));
-            context.setVariable("supplier", supplierDataStore.getAll());
-            System.out.println(productCategoryDataStore.getAll());
-            engine.process("product/index.html", context, response.getWriter());
-        }
-        else {
-            TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
-            WebContext context = new WebContext(request, response, request.getServletContext());
-            context.setVariable("category", productCategoryDataStore.getAll());
-            context.setVariable("supplier", supplierDataStore.getAll());
-            context.setVariable("products", productService.getProductsForSupplier(Integer.parseInt(supplier)));
-            engine.process("product/index.html", context, response.getWriter());
-            System.out.println(supplierDataStore.getAll());
-        }
-
     }
 
 }
