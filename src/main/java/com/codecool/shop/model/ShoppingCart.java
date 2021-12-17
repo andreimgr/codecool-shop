06872db6eb37e7124b75c285d7ef5a27dc2de50a
Andrei <andreim.graur@gmail.com>
2021-12-17 12:31:04 +0200

    Reworked shopping cart model

--- src/main/java/com/codecool/shop/model/ShoppingCart.java
+++ src/main/java/com/codecool/shop/model/ShoppingCart.java
@@ -1,55 +1,88 @@
 package com.codecool.shop.model;
 
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.List;
 
-public class ShoppingCart extends BaseModel {
-    public List<Product> productInShoppingCart = new ArrayList<>();
-    public int numOfItemsInShoppingCart = 0;
+public class ShoppingCart {
 
-    private int productId;
-    private int amount;
-    private float price;
+    private List<LineItem> lineItems = new ArrayList<>();
+    private Currency currency;
+    private float totalPrice;
+    private String subTotal;
 
-    public ShoppingCart(int productId, int amount, float price, String name) {
-        this.productId = productId;
-        this.amount = amount;
-        this.price = price;
-        this.name = name;
+    public ShoppingCart(Product product) {
+        this.currency = product.getDefaultCurrency();
+        addProduct(product);
     }
 
-    public int getProductId() {
-        return productId;
+    public void addProduct(Product product) {
+        LineItem lineItem;
+        if (getLineItemByProduct(product) != null) {
+            lineItem = getLineItemByProduct(product);
+            lineItem.increaseQuantity();
+        } else {
+            lineItem = new LineItem(product);
+            this.lineItems.add(lineItem);
+        }
+        totalPrice += lineItem.getProduct().getDefaultPrice();
+        this.subTotal = String.format("%.02f", totalPrice);
     }
 
-    public float getPrice() {
-        return price;
+    public void removeLineItem(int productId) {
+        LineItem lineItem = getLineItemByProductId(productId);
+        totalPrice -= lineItem.getPrice();
+        this.subTotal = String.format("%.02f", totalPrice);
+        this.lineItems.remove(lineItem);
+
+    }
+    public void decreaseLineItem(int productId) {
+        LineItem lineItem = getLineItemByProductId(productId);
+        if(lineItem.getQuantity() == 1){
+            removeLineItem(productId);
+        } else {
+            totalPrice -= lineItem.getProduct().getDefaultPrice();
+            lineItem.decreaseQuantity();
+        }
+        System.out.println(lineItem.getPrice());
+        this.subTotal = String.format("%.02f", totalPrice);
+    }
+    public void increaseLineItem(int productId) {
+        LineItem lineItem = getLineItemByProductId(productId);
+        lineItem.increaseQuantity();
+        totalPrice += lineItem.getProduct().getDefaultPrice();
+        this.subTotal = String.format("%.02f", totalPrice);
     }
 
-    public int getAmount() {
-        return amount;
+    public LineItem getLineItemByProduct(Product product) {
+        return lineItems.stream().filter(t -> t.getProduct().getId() == product.getId()).findFirst().orElse(null);
     }
 
-    public List<Product> getProductInShoppingCart() {
-        return productInShoppingCart;
+    public LineItem getLineItemByProductId(int productId) {
+        return lineItems.stream().filter(t -> t.getProduct().getId() == productId).findFirst().orElse(null);
+    }
+    public List<LineItem> getLineItems() {
+        return lineItems;
     }
 
-    public void setProductInShoppingCart(List<Product> productInShoppingCart) {
-        this.productInShoppingCart = productInShoppingCart;
+    public LineItem getLineItemsFirst() {
+        return lineItems.get(0);
     }
 
-    public void addToShoppingCart(Product productToAdd) {
-        //productToAdd.id = numOfItemsInShoppingCart;
-        numOfItemsInShoppingCart += 1;
-        productInShoppingCart.add(productToAdd);
+    public float getTotalPrice() {
+        return totalPrice;
     }
 
-    @Override
-    public String toString() {
-        return "ShoppingCart{" +
-                "productId=" + productId +
-                ", amount=" + amount +
-                ", price=" + price +
-                '}';
+    public void removeLineItem(LineItem lineItem) {
+        this.lineItems.remove(lineItem);
     }
+
+    public String getCurrency() {
+        return currency.toString();
+    }
+
+    public String getSubTotal() {
+        return subTotal;
+    }
+
 }
