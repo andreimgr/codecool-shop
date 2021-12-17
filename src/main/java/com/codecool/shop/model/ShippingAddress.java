0829e04e05eedec0db147fb3223a97cdd107d1e2
Andrei <andreim.graur@gmail.com>
2021-12-17 12:39:57 +0200

    Shipping address model added

---
+++ src/main/java/com/codecool/shop/model/ShippingAddress.java
@@ -0,0 +1,9 @@
+package com.codecool.shop.model;
+
+public class ShippingAddress extends Address {
+
+    public ShippingAddress(String country, String city, int zipcode, String street, int houseNumber) {
+        super(country, city, zipcode, street, houseNumber);
+    }
+
+}
